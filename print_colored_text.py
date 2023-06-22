1265083af5b2ef6b0efb8101d9367641171b0081
22 <87944616+tuvvshe@users.noreply.github.com>
2023-06-22 22:24:04 +0200

    Add files via upload

---
+++ print_colored_text.py
@@ -0,0 +1,8 @@
+
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Print Colored Text with Pycharm "+ Fore.YELLOW+ Back.BLUE+"Gerolsteiner Medium Naturlichen erfrischend und reich an Mineralien")
+print(Back.CYAN+"Gerolsteiner Medium Naturlichen erfrischend und reich an Mineralien")
+print(Fore.RED + Back.GREEN+ "Gerolsteiner Medium Naturlichen erfrischend und reich an Mineralien")
